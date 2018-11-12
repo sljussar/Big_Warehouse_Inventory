50395dc282a1c7e02d0cac65412e70492a8b4e1e
sljussar <44267231+sljussar@users.noreply.github.com>
2018-11-12 15:50:33 -0500

    Update conf.php

--- conf.php
+++ conf.php
@@ -4,11 +4,9 @@ $pgport="8889";
 $pghost="localhost";
 $db_user='root';
 $db_pass='root';
-//$schemas=array("saps"=>"public"); //nombre de los esquemas
 $xajax_path="../xajax"; //ruta del framework de xajax
-//date_default_timezone_set("America/Caracas");
 //sage
 $odbc='odbc:test';
-$sage_user='johana|EPI';
-$sage_pass='16153633';
+$sage_user='test_user|EPI';
+$sage_pass='1234';
 ?>
